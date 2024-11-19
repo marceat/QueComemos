7fd6b8c2b15497d4689082ef12d38b1cf884acbe
Marcelo <Marcelo@LenovoMarce>
2024-11-19 20:44:30 -0300

    Corregido error en los test.

--- src/main/java/spring/QueComemos/services/UsuarioGeneralDAOjpa.java
+++ src/main/java/spring/QueComemos/services/UsuarioGeneralDAOjpa.java
@@ -1,8 +1,11 @@
 package spring.QueComemos.services;
 
+import org.springframework.stereotype.Repository;
+
 import spring.QueComemos.dao.DaoGenerico;
 import spring.QueComemos.model.UsuarioGeneral;
 
+@Repository
 public class UsuarioGeneralDAOjpa extends GenericDaoImpJpa<UsuarioGeneral> implements DaoGenerico<UsuarioGeneral> {
 	
 	public UsuarioGeneralDAOjpa() {
--- src/main/java/spring/QueComemos/services/VentaDAOjpa.java
+++ src/main/java/spring/QueComemos/services/VentaDAOjpa.java
@@ -1,10 +1,13 @@
 package spring.QueComemos.services;
 
 
+import org.springframework.stereotype.Repository;
+
 import spring.QueComemos.dao.DaoGenerico;
 
 import spring.QueComemos.model.Venta;
 
+@Repository
 public class VentaDAOjpa extends GenericDaoImpJpa<Venta> implements DaoGenerico<Venta> {
 	
 	public VentaDAOjpa() {
